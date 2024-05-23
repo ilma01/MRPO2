86a5865549fe44bf676eeaab7f09c56393b6406b
ilma01 <mashukov2003@gmail.com>
2024-05-23 17:54:22 +0700

    PR4

--- Repository/SQLAlchRepos.py
+++ Repository/SQLAlchRepos.py
@@ -9,50 +9,6 @@ from ModelsBD import CarBrand, CarModel, Car, Consumable, Service, ServiceConsum
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-class ModelAdapter:
-    @staticmethod
-    def to_new_consumable(old_consumable: OldConsumable) -> Consumable:
-        return Consumable(
-            name=old_consumable.name,
-            manufacturer=old_consumable.manufacturer,
-            cost=old_consumable.cost
-        )
-    @staticmethod
-    def to_new_service(old_service: OldService) -> Service:
-        return Service(
-            id=old_service.id,
-            date=old_service.date,
-            service_type=old_service.service_type,
-            cost=old_service.cost,
-            car_vin=old_service.car.vin,
-            tasks=old_service.tasks,
-            mileage=old_service.mileage,
-            consumables=[ModelAdapter.to_new_consumable(consumable) for consumable in old_service.consumables]
-        )
-    @staticmethod
-    def to_old_service(new_service: Service) -> OldService:
-        if new_service:
-            old_service = OldService(
-                id=new_service.id,
-                date=new_service.date,
-                service_type=new_service.service_type,
-                cost=new_service.cost,
-                car=ModelAdapter.to_old_car(new_service.car),
-                tasks=new_service.tasks,
-                consumables=[ModelAdapter.to_old_consumable(consumable) for consumable in new_service.consumables]
-            )
-            old_service.mileage = new_service.mileage
-            return old_service
-        return None
-    @staticmethod
-    def to_old_consumable(new_consumable: Consumable) -> OldConsumable:
-         if new_consumable:
-            return OldConsumable(
-                name=new_consumable.name,
-                manufacturer=new_consumable.manufacturer,
-                cost=new_consumable.cost
-            )
-         return None
 class CarBrandRepos(FakeRepos):
     def to_new_car_brand(self, old_car_brand: OldCarBrand) -> CarBrand:
         return CarBrand(
@@ -86,7 +42,10 @@ class CarBrandRepos(FakeRepos):
         return list(map(self.to_old_car_brand, self.session.query(CarBrand).all()))
 
     def get(self, name):
-        return self.to_old_car_brand(self.session.query(CarBrand).filter_by(name=name).first())
+        ret = self.to_old_car_brand(self.session.query(CarBrand).filter_by(name=name).first())
+        if ret:
+            return ret
+        return None
 
 class CarModelRepos(FakeRepos):
     def to_new_car_model(self, old_car_model: OldCarModel) -> CarModel:
@@ -134,12 +93,15 @@ class CarModelRepos(FakeRepos):
         return list(map(self.to_old_car_model, self.session.query(CarModel).all()))
 
     def get(self, name):
-        return self.to_old_car_model(self.session.query(CarModel).filter_by(name=name).first())
+        ret = self.to_old_car_model(self.session.query(CarModel).filter_by(name=name).first())
+        if ret:
+            return ret
+        return None
 
 class CarRepos(FakeRepos):
-    def to_new_car(old_car: OldCar) -> Car:
+    def to_new_car(self, old_car: OldCar) -> Car:
         return Car(
-            model=ModelAdapter.to_new_car_model(old_car.model),
+            model=self.session.query(CarModel).filter_by(name=old_car.model.name).first(),
             mileage=old_car.mileage,
             year=old_car.year,
             vin=old_car.vin,
@@ -147,15 +109,35 @@ class CarRepos(FakeRepos):
             last_service_date=old_car.last_service_date
         )
 
-    def to_old_car(new_car: Car) -> OldCar:
+    def to_old_car(self, new_car: Car) -> OldCar:
         if new_car:
             return OldCar(
-                model=ModelAdapter.to_old_car_model(new_car.model),
+                model=self.to_old_car_model(new_car.model),
                 mileage=new_car.mileage,
                 year=new_car.year,
                 vin=new_car.vin,
                 last_service_mileage=new_car.last_service_mileage,
                 last_service_date=new_car.last_service_date
+            )
+        return None
+
+    def to_old_car_model(self, new_car_model: CarModel) -> OldCarModel:
+        if new_car_model:
+            return OldCarModel(
+                name=new_car_model.name,
+                brand=self.to_old_car_brand(new_car_model.brand),
+                year=new_car_model.year,
+                body_type=new_car_model.body_type,
+                engine_volume=new_car_model.engine_volume
+            )
+        return None
+
+    def to_old_car_brand(self, new_car_brand: CarBrand) -> OldCarBrand:
+        if new_car_brand:
+            return OldCarBrand(
+                name=new_car_brand.name,
+                country=new_car_brand.country,
+                founding_year=new_car_brand.founding_year
             )
         return None
 
@@ -176,44 +158,124 @@ class CarRepos(FakeRepos):
         return list(map(self.to_old_car, self.session.query(Car).all()))
 
     def get(self, vin):
-        if self.to_old_car(self.session.query(Car).filter_by(vin=vin).first()):
-            return self.to_old_car(self.session.query(Car).filter_by(vin=vin).first())
+        ret = self.to_old_car(self.session.query(Car).filter_by(vin=vin).first())
+        if ret:
+            return ret
         return None
 
 class ConsumableRepos(FakeRepos):
+    def to_new_consumable(self, old_consumable: OldConsumable) -> Consumable:
+        return Consumable(
+            name=old_consumable.name,
+            manufacturer=old_consumable.manufacturer,
+            cost=old_consumable.cost
+        )
+
+    def to_old_consumable(self, new_consumable: Consumable) -> OldConsumable:
+         if new_consumable:
+            return OldConsumable(
+                name=new_consumable.name,
+                manufacturer=new_consumable.manufacturer,
+                cost=new_consumable.cost
+            )
+         return None
+
     def add(self, consumable):
-        self.session.add(consumable)
+        self.session.add(self.to_new_consumable(consumable))
         self.session.commit()
 
     def remove(self, consumable):
-        self.session.delete(consumable)
+        self.session.delete(self.session.query(Consumable).filter_by(name=consumable.name).first())
         self.session.commit()
 
     def update(self, consumable):
-        self.session.merge(consumable)
+        self.session.merge(self.to_new_consumable(consumable))
         self.session.commit()
 
     def get_all(self):
-        return self.session.query(Consumable).all()
+        return list(map(self.to_old_consumable,self.session.query(Consumable).all()))
 
     def get(self, name):
-        return self.session.query(Consumable).filter_by(name=name).all()
+        ret = self.to_old_consumable(self.session.query(Consumable).filter_by(name=name).first())
+        if ret:
+            return ret
+        return None
 
 class ServiceRepos(FakeRepos):
+    def to_new_service(self, old_service: OldService) -> Service:
+        return Service(
+            id=old_service.id,
+            date=old_service.date,
+            service_type=old_service.service_type,
+            cost=old_service.cost,
+            car_vin=old_service.car.vin,
+            tasks=old_service.tasks,
+            mileage=old_service.mileage
+        )
+
+    def to_old_service(self, new_service: Service) -> OldService:
+        if new_service:
+            old_service = OldService(
+                id=new_service.id,
+                date=new_service.date,
+                service_type=new_service.service_type,
+                cost=new_service.cost,
+                car=self.to_old_car(self.session.query(Car).filter_by(vin=new_service.car_vin).first()),
+                tasks=new_service.tasks
+            )
+            old_service.mileage = new_service.mileage
+            return old_service
+        return None
+
+    def to_old_car(self, new_car: Car) -> OldCar:
+        if new_car:
+            return OldCar(
+                model=self.to_old_car_model(new_car.model),
+                mileage=new_car.mileage,
+                year=new_car.year,
+                vin=new_car.vin,
+                last_service_mileage=new_car.last_service_mileage,
+                last_service_date=new_car.last_service_date
+            )
+        return None
+
+    def to_old_car_model(self, new_car_model: CarModel) -> OldCarModel:
+        if new_car_model:
+            return OldCarModel(
+                name=new_car_model.name,
+                brand=self.to_old_car_brand(new_car_model.brand),
+                year=new_car_model.year,
+                body_type=new_car_model.body_type,
+                engine_volume=new_car_model.engine_volume
+            )
+        return None
+
+    def to_old_car_brand(self, new_car_brand: CarBrand) -> OldCarBrand:
+        if new_car_brand:
+            return OldCarBrand(
+                name=new_car_brand.name,
+                country=new_car_brand.country,
+                founding_year=new_car_brand.founding_year
+            )
+        return None
+
     def add(self, service):
-        self.session.add(service)
+        self.session.add(self.to_new_service(service))
         self.session.commit()
 
     def remove(self, service):
-        self.session.delete(service)
+        self.session.delete(self.session.query(Service).filter_by(id=service.id).first())
         self.session.commit()
 
     def update(self, service):
-        self.session.merge(service)
+        self.session.merge(self.to_new_service(service))
         self.session.commit()
 
     def get_all(self):
-        return self.session.query(Service).all()
+        return list(map(self.to_old_service,self.session.query(Service).all()))
 
     def get(self, service_id):
-        return self.session.query(Service).filter_by(id=service_id).first()
+        ret = self.session.query(Service).filter_by(id=service_id).first()
+        if ret:
+            return ret
+        return None
--- sqltest.py
+++ sqltest.py
@@ -11,19 +11,12 @@ session = Session()
 
 BrandRepos = CarBrandRepos(session)
 ModelRepos = CarModelRepos(session)
-#CarRepos = CarRepos(session)
-#ServiceRepos = ServiceRepos(session)
-#ConsRepos = ConsumableRepos(session)
+CarRepos = CarRepos(session)
+ConsRepos = ConsumableRepos(session)
+ServiceRepos = ServiceRepos(session)
 
-CarUC = CarProcedure.CarProcedure(CarRepos)
 BrandUC = CarBrandProcedure.CarBrandProcedure(BrandRepos)
-
-BrandUC.update_brand('Toyota', founding_year=1995)
-
-#BrandUC.remove_brand('Toyota')
-
-#CarUC.add_car(ModelUC.get_model('Supra'), vin='XTA342141')
-
-#ModelUC.add_model('Supra', BrandUC.get_brand('Toyota'), body_type='Sedan')
-#ModelUC.add_model('Impreza', BrandUC.get_brand('Subaru'))
-#ModelUC.update_model('Supra', body_type='Coupe')
+ModelUC = CarModelProcedure.CarModelProcedure(ModelRepos)
+CarUC = CarProcedure.CarProcedure(CarRepos)
+ConsUC = ConsumableProcedure.ConsumableProcedure(ConsRepos)
+ServiceUC = ServiceProcedure.ServiceProcedure(ServiceRepos)
